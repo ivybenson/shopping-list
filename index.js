f576bb699f4a5036b1fb2782bc3a091e9077af4d
ivybenson <68450827+ivybenson@users.noreply.github.com>
2020-07-30 20:05:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ function shoppingList (){
      e.preventDefault(); 
       const foodInput = $('#shopping-list-entry').val();
       ('#shopping-list-entry').val('');
-      ('.container').append('<li>${foodInput}');
-      });
+
+      ('.shopping-list').append('<li>
+        <span class="shopping-item">${foodInput}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      ');
+         });
+
     
     $('.shopping-item-delete').on('click','button',function(e){
       e.preventDefault();
